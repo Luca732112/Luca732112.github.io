e9fedc83c9d9ac7ab8af9e68f6a40dd19b4d10e6
Luca <159593445+Luca732112@users.noreply.github.com>
2024-06-17 19:42:36 -0600

    practica 12

---
+++ spotify (1).sql
@@ -0,0 +1,432 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-06-2024 a las 03:41:28
+-- Versión del servidor: 10.1.39-MariaDB
+-- Versión de PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `spotify`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artists`
+--
+
+CREATE TABLE `artists` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `descripcion` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `artists`
+--
+
+INSERT INTO `artists` (`id`, `name`, `descripcion`) VALUES
+(1, 'Queen', 'Queen es una banda británica de rock formada en 1970 en Londres, integrada originalmente por el cant'),
+(2, 'David Bowie', 'David Robert Jones, ​​ más conocido por su nombre artístico David Bowie, ​ fue un cantautor, actor, '),
+(3, 'The Beatles', 'The Beatles, más conocido en el mundo hispano como los Beatles, fue un grupo de rock británico forma'),
+(4, 'Coldplay', 'Coldplay es una banda británica de rock formada en Londres en 1997. Está integrada por Chris Martin,'),
+(6, 'The Jackson 5', 'The Jackson 5, desde 1975, The Jacksons, es una boy band estadounidense integrada anteriormente por '),
+(7, 'Abba', 'ABBA es un grupo sueco de música pop, integrado por Agnetha Fältskog, Björn Ulvaeus, Benny Andersson'),
+(8, 'Imagine Dragons', 'Imagine Dragons es una banda estadounidense de pop rock originaria de Las Vegas, Nevada. Está compue'),
+(9, 'Earth, Wind & Fire', 'Earth, Wind & Fire, conocidos también como EWF, es un grupo musical estadounidense, formado en Chica'),
+(10, 'Elton John', 'Elton Hercules John es un cantautor, músico y pianista de rock y pop británico.​​​ Con una carrera d'),
+(11, 'BoyWithUke', 'Charley Yang, conocido profesionalmente como BoyWithUke, es un cantante, músico y personalidad de In'),
+(12, 'El Cuarteto de Nos', 'El Cuarteto de Nos es una banda de rock uruguaya formada en Montevideo en 1984. Está integrada por e');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artists_songs`
+--
+
+CREATE TABLE `artists_songs` (
+  `id` int(11) NOT NULL,
+  `artists_id` int(11) NOT NULL,
+  `songs_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `artists_songs`
+--
+
+INSERT INTO `artists_songs` (`id`, `artists_id`, `songs_id`) VALUES
+(1, 1, 2),
+(2, 3, 6),
+(3, 2, 3),
+(4, 3, 5),
+(5, 1, 1),
+(6, 2, 4),
+(7, 12, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favoritos`
+--
+
+CREATE TABLE `favoritos` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `songs_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `favoritos`
+--
+
+INSERT INTO `favoritos` (`id`, `user_id`, `songs_id`) VALUES
+(1, 1, 2),
+(2, 1, 3),
+(3, 5, 3),
+(4, 2, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `genre`
+--
+
+CREATE TABLE `genre` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `genre`
+--
+
+INSERT INTO `genre` (`id`, `name`) VALUES
+(1, 'Rock n Roll'),
+(2, 'Pop'),
+(3, 'R\'n\'B​'),
+(4, 'Disco'),
+(5, 'Heavy Metal'),
+(6, 'Funk'),
+(7, 'Soul'),
+(8, 'Glam Rock');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `memberships`
+--
+
+CREATE TABLE `memberships` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `precio` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `memberships`
+--
+
+INSERT INTO `memberships` (`id`, `name`, `precio`) VALUES
+(1, 'Premium', 0),
+(2, 'Freemium', 0),
+(3, 'Familiar', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `playlist`
+--
+
+CREATE TABLE `playlist` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `duration` time NOT NULL,
+  `total_can` int(11) NOT NULL,
+  `description` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `playlist`
+--
+
+INSERT INTO `playlist` (`id`, `name`, `user_id`, `duration`, `total_can`, `description`) VALUES
+(1, 'Awesome Mix', 1, '06:18:00', 3, 'Best Music of all the times');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `playlist_songs`
+--
+
+CREATE TABLE `playlist_songs` (
+  `id` int(11) NOT NULL,
+  `playlist_id` int(11) NOT NULL,
+  `songs_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `playlist_songs`
+--
+
+INSERT INTO `playlist_songs` (`id`, `playlist_id`, `songs_id`, `user_id`) VALUES
+(1, 1, 2, 1),
+(2, 1, 1, 1),
+(3, 1, 3, 1),
+(4, 1, 4, 1),
+(5, 1, 5, 1),
+(6, 1, 6, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `songs`
+--
+
+CREATE TABLE `songs` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `genero_id` int(11) NOT NULL,
+  `duration` time NOT NULL,
+  `fecha` date NOT NULL,
+  `active` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `songs`
+--
+
+INSERT INTO `songs` (`id`, `name`, `genero_id`, `duration`, `fecha`, `active`) VALUES
+(1, 'Stone Cold Crazy', 1, '02:13:00', '1974-11-08', 1),
+(2, 'Hammer to Fall', 5, '04:26:00', '1984-09-10', 1),
+(3, 'Starman', 2, '04:14:00', '1972-04-03', 1),
+(4, 'Moonage Daydream', 1, '04:39:00', '1972-04-03', 1),
+(5, 'Eleanor Rigby', 2, '02:06:00', '1966-08-08', 1),
+(6, 'Hey Jude', 2, '03:57:00', '1968-08-26', 1),
+(7, 'Ya no sé que hacer conmigo', 1, '00:04:01', '2007-04-04', 1),
+(8, 'Til Kingdom Come', 1, '04:11:00', '2005-06-06', 1),
+(9, 'Clocks', 1, '04:10:00', '2003-03-24', 1),
+(10, 'I Want You Back', 2, '03:20:00', '1997-05-26', 1),
+(11, 'ABC', 2, '02:55:00', '1970-02-24', 1),
+(12, 'Dancing Queen', 2, '03:50:00', '1976-08-15', 1),
+(13, 'Mamma Mia', 2, '03:32:00', '1975-06-12', 1),
+(14, 'Believer ', 1, '03:24:00', '2017-02-01', 1),
+(15, 'Radioactive', 1, '00:20:12', '0000-00-00', 1),
+(16, 'September', 4, '03:35:00', '1978-11-18', 1),
+(17, 'Boogie Wonderland', 6, '04:48:00', '1979-03-20', 1),
+(18, 'Rocketman', 2, '04:42:00', '1972-04-17', 1),
+(19, 'Your Song', 2, '04:03:00', '1970-10-26', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `correo` varchar(150) NOT NULL,
+  `memberships_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `correo`, `memberships_id`) VALUES
+(1, 'Luca', 'lacorreag12@gmail.com', 1),
+(2, 'Kris', 'stakataka3000@gmail.com', 2),
+(3, 'cesar', 'Cesarino@gmail.com', 1),
+(4, 'Alberto', 'alberthanos@gmail.com\r\n', 2),
+(5, 'Mario', 'tecmario@gmail.com', 1),
+(6, 'Sonia', 'sngomezm@gmail.com', 3);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `artists`
+--
+ALTER TABLE `artists`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `artists_songs`
+--
+ALTER TABLE `artists_songs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artists_id` (`artists_id`),
+  ADD KEY `songs_id` (`songs_id`);
+
+--
+-- Indices de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `songs_id` (`songs_id`);
+
+--
+-- Indices de la tabla `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `memberships`
+--
+ALTER TABLE `memberships`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `playlist`
+--
+ALTER TABLE `playlist`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indices de la tabla `playlist_songs`
+--
+ALTER TABLE `playlist_songs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `playlist_id` (`playlist_id`),
+  ADD KEY `songs_id` (`songs_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indices de la tabla `songs`
+--
+ALTER TABLE `songs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `genero_id` (`genero_id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `memberships_id` (`memberships_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `artists`
+--
+ALTER TABLE `artists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `artists_songs`
+--
+ALTER TABLE `artists_songs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `memberships`
+--
+ALTER TABLE `memberships`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `playlist`
+--
+ALTER TABLE `playlist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `playlist_songs`
+--
+ALTER TABLE `playlist_songs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `songs`
+--
+ALTER TABLE `songs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `artists_songs`
+--
+ALTER TABLE `artists_songs`
+  ADD CONSTRAINT `artists_songs_ibfk_1` FOREIGN KEY (`artists_id`) REFERENCES `artists` (`id`),
+  ADD CONSTRAINT `artists_songs_ibfk_2` FOREIGN KEY (`songs_id`) REFERENCES `songs` (`id`);
+
+--
+-- Filtros para la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `favoritos_ibfk_2` FOREIGN KEY (`songs_id`) REFERENCES `songs` (`id`);
+
+--
+-- Filtros para la tabla `playlist`
+--
+ALTER TABLE `playlist`
+  ADD CONSTRAINT `playlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Filtros para la tabla `playlist_songs`
+--
+ALTER TABLE `playlist_songs`
+  ADD CONSTRAINT `playlist_songs_ibfk_1` FOREIGN KEY (`playlist_id`) REFERENCES `playlist` (`id`),
+  ADD CONSTRAINT `playlist_songs_ibfk_2` FOREIGN KEY (`songs_id`) REFERENCES `songs` (`id`),
+  ADD CONSTRAINT `playlist_songs_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Filtros para la tabla `songs`
+--
+ALTER TABLE `songs`
+  ADD CONSTRAINT `songs_ibfk_1` FOREIGN KEY (`genero_id`) REFERENCES `genre` (`id`);
+
+--
+-- Filtros para la tabla `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`memberships_id`) REFERENCES `memberships` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
